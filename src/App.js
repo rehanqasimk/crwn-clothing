dd982bc3db97894f67f41f6c82354216f16bb589
Rehan Qasim <qkrehan@gmail.com>
2022-10-30 18:04:08 +0000

    <Outlet /> in the react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home";
+
+const Shop = () => {
+  return <h1>I am the Shops</h1>;
+};
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home />}>
+        <Route path="/shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
