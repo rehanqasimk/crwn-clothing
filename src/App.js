66c4980509d0bd2076ab4fd16b323442d01b78e2
Rehan Qasim <qkrehan@gmail.com>
2022-10-30 18:20:00 +0000

    Making Navigation bar constant

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import Home from "./routes/home";
 
+const Navigation = () => {
+  return (
+    <div>
+      <h1>I am the Navigation Bar</h1>
+      <Outlet />
+    </div>
+  );
+};
 const Shop = () => {
-  return <h1>I am the Shops</h1>;
+  return <h1>I am the Shop</h1>;
 };
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />}>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
         <Route path="/shop" element={<Shop />} />
       </Route>
     </Routes>
